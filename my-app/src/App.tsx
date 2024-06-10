2f27dd4c6f4459491e0b2f112e3ce94ae1b1c4bf
sakin333 <beast.glitch3@gmail.com>
2024-06-10 20:52:18 +0545

    refactor: remove unnecessary imports

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Navbar from "./components/navbar";
 import Hero from "./pages/homepage/hero";
 import Explore from "./pages/featurepage/explore";
--- my-app/src/pages/earnpage/earn-section.tsx
+++ my-app/src/pages/earnpage/earn-section.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "../../style";
 import ExploreImage from "../../assets/explore-img.png";
 
